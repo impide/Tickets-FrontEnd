1ba2479f104e730ad24b81551f1e1182c913c1a8
Tony Petrasinovic <tonypetrasinovic@gmail.com>
2024-04-30 22:47:33 +0200

    init vue front + login + register+nav+

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import { Modal } from 'flowbite'
+import LoadScript from "vue-plugin-load-script";
+import type { ModalOptions, ModalInterface } from 'flowbite'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(LoadScript).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+<h2 class="text-2xl text-gray-950 mt-80">Home</h2>
+
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+
+</script>
