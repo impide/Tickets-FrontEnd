61dc64b759444dabe14b475a0cc380c5c72a2ebd
Tony Petrasinovic <tonypetrasinovic@gmail.com>
2024-05-05 23:10:34 +0200

    unit test

--- tests/nav.test.js
+++ tests/nav.test.js
@@ -8,25 +8,62 @@ jest.mock("vue-router", () => ({
         push: jest.fn().mockResolvedValue(),
     }),
 }));
-jest.mock("vuex", () => ({
-    useStore: () => ({
-        state: { authenticated: false },
-        dispatch: jest.fn(),
-    }),
-}));
+
+jest.mock("vuex");
 
 describe("Nav.vue", () => {
-    it('should  contain "Connexion" and "Inscription" links when user is not authenticated', () => {
+    it('should not contain "Connexion" and "Inscription" links when user is authenticated', () => {
+
+        useStore.mockImplementation(() => ({
+            state: { authenticated: true },
+        }));
 
         const wrapper = shallowMount(Nav);
 
         expect(wrapper.exists()).toBe(true);
 
 
-        const loginLink = wrapper.find('router-link[to="/login"]');
-        const registerLink = wrapper.find('router-link[to="/register"]');
+        const loginLink = wrapper.find('#login');
+        const registerLink = wrapper.find('#register');
+
+        expect(loginLink.exists()).toBe(false);
+        expect(registerLink.exists()).toBe(false);
+    });
+    it('should contain "Connexion" and "Inscription" links when user is not authenticated', () => {
+        useStore.mockImplementation(() => ({
+            state: { authenticated: false },
+        }));
+
+        const wrapper = shallowMount(Nav);
+
+        expect(wrapper.exists()).toBe(true);
+
+
+        const loginLink = wrapper.find('#login');
+        const registerLink = wrapper.find('#register');
 
         expect(loginLink.exists()).toBe(true);
         expect(registerLink.exists()).toBe(true);
     });
+    it('should call logout on setup method', () => {
+        const wrapper = shallowMount(Nav);
+        expect(wrapper.vm.logout).toBeDefined();
+
+    });
+
+    it('should call logout on click button', () => {
+        useStore.mockImplementation(() => ({
+            state: { authenticated: true },
+        }));
+
+        const wrapper = shallowMount(Nav);
+
+        const logoutButton = wrapper.find('#logout');
+        logoutButton.trigger('click');
+        const spy = jest.spyOn(wrapper.vm, 'logout');
+        expect(spy).toHaveBeenCalled();
+    });
+
+    // it('should remove authToken and userId on local storage', () => {
+    // });
 });
